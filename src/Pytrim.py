b7607f9f39f6f08bbc2953a2dc5536c227edc280
aswolff <aswolff7@gmail.com>
2024-04-04 16:30:21 -0400

    Made code simpler

--- src/Pytrim.py
+++ src/Pytrim.py
@@ -1,2 +1,16 @@
-class Pytrim:
-    def __init__(self, window):
+from moviepy.editor import *
+import sys
+import os.path
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 3:
+        print("Usage: Pytim.py <filename> <start> <end>")
+    if os.path.isfile(sys.argv[1]):
+        video = sys.argv[1]
+    else:
+        print(f'Video file does not exist. Please confirm that you are using {sys.argv[1]}')
+    start, end = sys.argv[2], sys.argv[3]
+    clip = VideoFileClip(video)
+    max_duration = clip.duration
+    clip = clip.subclip(start, end)
+    clip.write_videofile('../clips/trimmed_video.mp4')
